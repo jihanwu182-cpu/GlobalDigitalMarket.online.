8964ec974046409a9e56116c7cc86f554068a14a
jihanwu <jihanwu182@gmail.com>
2026-08-25 22:40:24 +0200

    Update index.tsx
    Signed-off-by: jihanwu <jihanwu182@gmail.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import store from './store';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Root element not found');
+}
+
+const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
