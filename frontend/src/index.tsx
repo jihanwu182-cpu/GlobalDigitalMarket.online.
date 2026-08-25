89fb74072b849087f1959654c103d9f89baacb3f
jihanwu <jihanwu182@gmail.com>
2026-08-25 23:09:29 +0200

    Update index.tsx
    Signed-off-by: jihanwu <jihanwu182@gmail.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import store from './store';
 
 const rootElement = document.getElementById('root');
 
@@ -13,6 +15,8 @@ const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
