02750d4c731e04e913cc84732b2ded85dbb9e9bc
jihanwu <jihanwu182@gmail.com>
2026-08-26 09:45:57 +0200

    Update index.tsx
    Signed-off-by: jihanwu <jihanwu182@gmail.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './index.css';
-import App from './app';
+import App from './App';
 import store from './store';
 
 const rootElement = document.getElementById('root');
